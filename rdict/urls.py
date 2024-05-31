65680ab6059b5f27c2e2ea34029a0050db8bf9b6
slocksert <rsdlj01@gmail.com>
2024-05-31 12:00:52 -0300

    added views

--- rdict/urls.py
+++ rdict/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app.views import home
+from app.views import home, saved_words
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home)
+    path('', home),
+    path('words', saved_words),
+    path('word/<str:word>/', home)
 ]
